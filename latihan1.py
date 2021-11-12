1d88bc3e6be16be37258892e8d5051e7963328e2
Billy Alfauzi <dangoekeunsajah@gmail.com>
2021-11-12 14:28:14 +0700

    menambahkan file latihan1.py

---
+++ latihan1.py
@@ -0,0 +1,13 @@
+#program mencari nilai n lebih daripada 0,5
+
+print(10*":)")
+import random
+jumlah = 5
+a = 0
+for x in range (jumlah):
+    i = random.uniform (.0,.5)
+    a+=1
+    print("data ke=" ,a,"===>",i)
+    print("selesai")
+
+    print(10*":)")
